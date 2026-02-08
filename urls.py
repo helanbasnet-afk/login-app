b2eb9fc69b4df70b5295f6935937e9aa5161706c
helanbasnet-afk <sanadeepbasnet@gmail.com>
2026-02-08 17:31:51 +0545

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('admin/', admin.site.urls),
+    path('', include('loginapp.urls')),
 ]
